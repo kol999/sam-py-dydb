940fe3d094e0690f11ff637ca883ba305b853cc5
kol <kol@work.com>
2022-11-17 23:01:45 +0000

    Added new fields.

--- README.md
+++ README.md
@@ -1,38 +1,53 @@
 # Amazon API Gateway to AWS Lambda to Amazon DynamoDB
 
-This pattern explains how to deploy a SAM application with Amazon API Gateway, AWS Lambda, and Amazon DynamoDB. When an HTTP POST request is made to the Amazon API Gateway endpoint, the AWS Lambda function is invoked and inserts an item into the Amazon DynamoDB table.
-
-Learn more about this pattern at Serverless Land Patterns: https://serverlessland.com/patterns/apigw-lambda-dynamodb
-
-Important: this application uses various AWS services and there are costs associated with these services after the Free Tier usage - please see the [AWS Pricing page](https://aws.amazon.com/pricing/) for details. You are responsible for any AWS costs incurred. No warranty is implied in this example.
-
-## Requirements
-
-* [Create an AWS account](https://portal.aws.amazon.com/gp/aws/developer/registration/index.html) if you do not already have one and log in. The IAM user that you use must have sufficient permissions to make necessary AWS service calls and manage AWS resources.
-* [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) installed and configured
-* [Git Installed](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
-* [AWS Serverless Application Model](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html) (AWS SAM) installed
+API Gateway -> Lambda -> DynamoDB
 
 ## Deployment Instructions
+From the root directory
+    ```
+    sam deploy --guided  
+    ```
+The above will create a config (samconfig.toml) for the respective environment. 
 
-1. Create a new directory, navigate to that directory in a terminal and clone the GitHub repository:
-    ``` 
-    git clone https://github.com/aws-samples/serverless-patterns
+All subsequent deployments can use the config file as follows (to use the test environment): 
     ```
-1. Change directory to the pattern directory:
+    sam deploy --config-env test
     ```
-    cd apigw-lambda-dynamodb
-    ```
-1. From the command line, use AWS SAM to deploy the AWS resources for the pattern as specified in the template.yml file:
-    ```
-    sam deploy --guided
-    ```
-1. During the prompts:
-    * Enter a stack name
-    * Enter the desired AWS Region
-    * Allow SAM CLI to create IAM roles with the required permissions.
 
-    Once you have run `sam deploy --guided` mode once and saved arguments to a configuration file (samconfig.toml), you can use `sam deploy` in future to use these defaults.
+When running locally. Start up the dynamodb instance 
+    ```
+    docker-compose up  
+    ```
+Environment parameters can be placed in the config file as follows:
+
+    ```
+    sam local start-api --env-vars env.json
+    ```
+
+env.json 
+   ```
+{
+    "Parameters": {
+        "AWSENV": "AWS_SAM_LOCAL"
+    }
+}
+   ```
+
+Fire off a request to the local endpoint as follows 
+   ```
+    curl -d "@event.json" -X POST http://localhost:3000
+   ```
+where event.json is  
+```
+{
+	"bookingID" : 33972,
+    "car" : "Toyota",
+	"startDate" : "2023-05-01", 
+	"endDate" : "2023-05-10", 
+	"bookingStatus" : "PAID"
+}
+```
+
 
 1. Note the outputs from the SAM deployment process. These contain the resource names and/or ARNs which are used for testing.
 
--- src/index.py
+++ src/index.py
@@ -1,52 +1,62 @@
 import boto3
 import json
-import logging 
+import logging
 import os
-import base64
-import socket
-
+import time
 from botocore.exceptions import ClientError
 
-#dynamodb_client = boto3.client('dynamodb',endpoint_url='http://localhost:8000')
+# dynamodb_client = boto3.client('dynamodb',endpoint_url='http://localhost:8000')
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-logger.setLevel(logging.INFO) 
 
 
 def lambda_handler(event, context):
-    try: 
-#         print(json.dumps(event, indent=2))
-        if event.get('body'):
-            requestBody = json.loads(event['body']) 
-            bookingID = requestBody['bookingID']
+    try:
+        #          print(json.dumps(event, indent=2))
+        if event.get("body"):
+            requestBody = json.loads(event["body"])
+            bookingID = requestBody["bookingID"]
             car = requestBody["car"]
+            #  startHireDate = requestBody["startDate"]
+            #  endHireDate = requestBody["endDate"]
+            status = requestBody["bookingStatus"]
+            startHireDate = "2023-05-03"
+            endHireDate = "2023-05-09"
         else:
             return {
-                'statusCode': 200,
+                'statusCode': 400,
                 'body': 'Error: incorrect body passed'
             }
-        logger.info(f"Adding bookingID {bookingID} and car {car}") 
-        response = updateTable(bookingID, car)
+        logger.info(f"Adding bookingID {bookingID} and car {car}")
+        return updateTable(bookingID, car, startHireDate, endHireDate, status)
     except ClientError as err:
-        logger.error("Unexpected error: {err}.")
-        
-    return response 
+        logger.error(f"ClientError: Unexpected error: {err}.")
+    except Exception as e:
+        logger.error(f"Exception: Unexpected exception: {e}.")
 
-def updateTable(bookingID, car): 
+    return {
+        'statusCode': 400,
+        'body': 'Lambda has thrown an error'
+    }
+
+
+def updateTable(bookingID, car, startHireDate, endHireDate, bookingStatus):
 
     table_name = os.environ.get('TABLE', 'HireCarTable')
     region = os.environ.get('REGION', 'eu-west-1')
     aws_environment = os.environ.get('AWSENV', 'AWS')
-    hostname = os.environ.get('hostname')
 
-    # The return code 
-    rc = 0 
-    returnBody = "" 
-
+    #  The return code
+    rc = 0
+    returnBody = ""
+    print(f"aws_environment: {aws_environment}")
     if aws_environment == 'AWS_SAM_LOCAL':
-        local_ip = get_localip() 
+        local_ipaddress = os.environ.get('LocalHostIP')
+        print(f"local_ipaddress: {local_ipaddress}")
+
         ddb = boto3.resource(
             'dynamodb',
-            endpoint_url="http://{local_ip}:8000"
+            endpoint_url=f"http://{local_ipaddress}:8000"
         )
     else:
         ddb = boto3.resource(
@@ -54,42 +64,40 @@ def updateTable(bookingID, car):
             region_name=region
         )
 
-    try:  
+    try:
         table = ddb.Table(table_name)
         params = {
-            'bookingID' : bookingID,
-            'car' : car
+            'bookingID': bookingID,
+            'car': car,
+            'startHireDate': startHireDate,
+            'endHireDate': endHireDate,
+            'status': bookingStatus,
+            'timestamp': str(int(time.time()))
         }
 
         response = table.put_item(
             TableName=table_name,
             Item=params
         )
-        rc =  response['ResponseMetadata']['HTTPStatusCode']  
+        rc = response['ResponseMetadata']['HTTPStatusCode']
         returnBody = 'Successfully inserted data!'
     except ClientError as err:
         logger.error(f"Error adding to table.{err}")
-        returnBody = err 
-        rc = response['ResponseMetadata']['HTTPStatusCode']  
-
+        returnBody = err
+        rc = response['ResponseMetadata']['HTTPStatusCode']
 
     return {
         'statusCode': rc,
-        'body': returnBody  
+        'body': returnBody
     }
 
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(0)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.254.254.254', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
 
-
-
+if __name__ == "__main__":
+    event = {
+        "body": "{\"bookingID\": \"1\", \"car\": \"Tesla\", \"startDate\": \"2023-05-03\", \"endDate\": \"2023-05-09\", \"bookingStatus\": \"pending\"}"
+    }
+    lambda_handler(event, {})
+    #  updateTable("1", "Tesla", "2023-05-03", "2023-05-09", "pending")
+    #  updateTable("2", "Tesla", "2023-05-03", "2023-05-09", "pending")
+    #  updateTable("3", "Tesla", "2023-05-03", "2023-05-09", "pending")
+    #  updateTable("4", "Tesla", "2023-05-03", "2023-05-09", "pending")
